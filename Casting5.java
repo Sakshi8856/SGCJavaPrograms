cd34080bdd87b14f9407a0c39a57d4ce7eb929db
Sakshi8856 <sc0682981@gmail.com>
2024-09-30 23:26:51 +0530

    created chapte2 inheritance repo

--- Casting5.java
+++
@@ -1,9 +0,0 @@
-
-public class Casting5 {
-    public static void main(String[] args) {
-        int num = 10;
-        double result = num;  // Implicit casting from int to double
-        System.out.println("Double value: " + result);
-    }
-
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# SGCJavaPrograms
-# SGCJavaPrograms
